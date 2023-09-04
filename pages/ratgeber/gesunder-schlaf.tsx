b8d0b800bd43651c70bdf2e2082802b8d77ff53c
sierra-mk <91697643+Marco-Kap@users.noreply.github.com>
2023-09-04 07:57:01 +0000

    Update gesunder-schlaf.tsx

--- pages/ratgeber/gesunder-schlaf.tsx
+++ pages/ratgeber/gesunder-schlaf.tsx
@@ -1,15 +1,22 @@
 import Banner from "/components/Banner";
 import BSNav from "/components/BSNav";
 import Footer from "/components/Footer";
+import Image from "next/image"
 
 export default function GesunderSchlaf() {
   const src = "";
+  const alt = ""
 
   return (
     <>
       <BSNav />
       <Banner source={src} alt="" text="" />
-      <main></main>
+      <main>
+        <div>
+
+        </div>
+        <Image width={400}  height={320} alt={alt} src={src}/>
+      </main>
       <Footer />
     </>
   );
