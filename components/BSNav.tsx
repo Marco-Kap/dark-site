0b5e9efeb9b9173f5d62d9127535f5410b6ad84f
sierra-mk <91697643+Marco-Kap@users.noreply.github.com>
2023-09-07 09:30:11 +0000

    Update 2 files

--- components/BSNav.tsx
+++ components/BSNav.tsx
@@ -73,7 +73,7 @@ export default function BSNav(props: NavProps) {
             </svg>
             Bahnhof Straße 7, 34497 Korbach
           </span>
-          <span>
+          <span style={{whiteSpace:"nowrap"}}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -91,12 +91,13 @@ export default function BSNav(props: NavProps) {
             <a
               style={{
                 color: "grey",
+                marginRight: ".5em"
               }}
               href="tel:056314040"
             >
               05631 4040
             </a>
-          </span>{" "}
+          </span>{" "}<span style={{whiteSpace: "nowrap"}}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -107,7 +108,7 @@ export default function BSNav(props: NavProps) {
           >
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
           </svg>
-          {isOpen()}
+          {isOpen()}</span>
         </div>
       </Container>
       <Container className="justify-items-start sticky-top bg-light bg rounded">
